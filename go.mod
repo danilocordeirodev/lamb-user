6a4ccddc5ea254b3d7154db568b8bb3ea74e0388
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-03-07 16:37:26 -0300

    add read secret

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.28.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.20.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.3 // indirect
--- main.go
+++ main.go
@@ -9,19 +9,40 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/danilocordeirodev/lamb-user/awsgo"
+	"github.com/danilocordeirodev/lamb-user/db"
+	"github.com/danilocordeirodev/lamb-user/models"
 )
 
 func main() {
 	lambda.Start(ExecuteLambda)
 }
 
-func ExecuteLambda(ctx context.Context, events events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error){
+func ExecuteLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error){
 	awsgo.InitializeAWS()
 
 	if !ValidateParameters() {
 		fmt.Println("Error in parameters. Should send 'SecretManager'")
 		err := errors.New("Error in parameters. Should send 'SecretManager'")
-		return events, err
+		return event, err
+	}
+
+	var data models.SignUp
+
+	for row, att := range event.Request.UserAttributes {
+		switch row {
+		case "email":
+			data.UserEmail = att
+			fmt.Println("Email = " + data.UserEmail)
+		case "sub":
+			data.UserUUID = att
+			fmt.Println("Sub = " + data.UserUUID)
+		}
+	}
+
+	err := db.ReadSecret()
+	if err != nil {
+		fmt.Println("Error to read secret " + err.Error())
+		return event, err
 	}
 }
 
